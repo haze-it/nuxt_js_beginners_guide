e13a92cb1164de2954e7f3eb00208e54e8f72d0d
Sho Tokuda <shoou0000@gmail.com>
2019-09-28 19:54:27 +0900

    02: Nuxt.jsによるシンプルなアプリケーション開発

---
+++ my-first-nuxt-app/pages/users/_id.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>{{user.id}}</h3>
+      <img :src="user.profile_image_url" width="120" alt="">
+      <p>{{user.description || 'no description'}}</p>
+      <p>
+        <nuxt-link to="/"><small>トップに戻る</small></nuxt-link>
+      </p>
+      <h3>{{user.id}} の投稿一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>{{item.title}}</h4>
+          <div>{{item.body.slice(0, 130)}}...</div>
+          <p><a target="_blank" :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+// export default {
+//   head() {
+//     return {
+//       title: this.user.id
+//     }
+//   },
+//   async asyncData({ route, app}) {
+//     const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
+//     const items =
+//       await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
+//     return { user, items}
+//   }
+// }
+import { mapGetters } from 'vuex'
+
+export default {
+  head() {
+    return {
+      title: this.user.id
+    }
+  },
+  async asyncData({ route, store, redirect }) {
+    if (store.getters['users'][route.params.id]) {
+      return
+    }
+    try {
+      await store.dispatch('fetchUserInfo', { id: route.params.id })
+    } catch (e) {
+      console.log('404')
+      console.log(e)
+      console.log(`id: ${route.params.id}`)
+      redirect('/') // 404
+    }
+  },
+  computed: {
+    user() {
+      return this.users[this.$route.params.id]
+    },
+    items() {
+      return this.userItems[this.$route.params.id] || []
+    },
+    ...mapGetters(['users', 'userItems'])
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 16px;
+}
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #ee55ee;
+}
+li + li {
+  margin: 16px 0;
+}
+p {
+  margin: 8px 0;
+}
+</style>
